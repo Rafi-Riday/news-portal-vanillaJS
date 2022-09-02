5b20eea9848b3375d7ad7f28d8c53ac2742a99f3
Rafiul Hossain Riday <Rafi-Riday>
2022-09-03 05:34:12 +0600

    added dynamic title on catagory option list btn

--- js/category.js
+++ js/category.js
@@ -4,4 +4,5 @@ const categoryBtnActivate = (activeBtn) => {
         categoryBtn.classList.remove('btn-info');
     }
     activeBtn.classList.add('btn-info');
+    document.title = `Daily-Dose | ${activeBtn.innerText}`;
 }
