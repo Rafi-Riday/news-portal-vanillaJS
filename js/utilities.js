a3c32711c0010409a893da530ed2d7f4cb02ff4a
Rafiul Hossain Riday <Rafi-Riday>
2022-09-02 21:32:12 +0600

    added tailwind, daisyUI, poppins font, berkshire font, font awesome

---
+++ js/utilities.js
@@ -0,0 +1,3 @@
+// const months = [{name: 'March', serial:3,}, {name: 'Jan', serial:1,}, {name: 'Feb', serial:2,}, {name: 'Dec', serial:12,}];
+// months.sort((a,b) => a.serial - b.serial);
+// console.log(months);
