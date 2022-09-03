2b630c881a0ea3e3ba90dd61e97816e6965d2ad6
Rafiul Hossain Riday <Rafi-Riday>
2022-09-04 01:00:51 +0600

    updated navbar style

--- js/global-variable.js
+++ js/global-variable.js
@@ -1,1 +1,17 @@
+fetch('https://openapi.programming-hero.com/api/news/category/08')
+    .then(res => res.json())
+    .then(data => {
+        const newsCount = data.data.length;
+        for (const newsCountContainer of document.getElementsByClassName('news-count-container')) {
+            newsCountContainer.innerText = `${newsCount}`;
+        }
+    })
+
+// const blogCount = document.getElementById('blog-container')?.childElementCount;
+const blogCountContainerList = document.getElementsByClassName('blog-count-container');
+for (const blogCountContainer of blogCountContainerList) {
+    // blogCountContainer.innerText = `${blogCount}`;
+    blogCountContainer.innerText = `${4}`;
+}
+
 const main = document.getElementById('main');
